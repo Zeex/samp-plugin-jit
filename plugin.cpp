10fd2db649069da1921359739a3a3c6a859eaf0b
Zeex <zeex@rocketmail.com>
2012-03-02 07:02:15 +0000

    Replace Hook() with JumpX86

--- plugin.cpp
+++ plugin.cpp
@@ -25,50 +25,13 @@
 #include <map>
 
 #include "jit.h"
+#include "jump-x86.h"
 #include "plugin.h"
 #include "pluginversion.h"
-
-#if defined LINUX
-	#include <sys/mman.h>
-#else
-	#include <Windows.h> 
-#endif
 
 using namespace jit;
 
 typedef void (*logprintf_t)(const char *format, ...);
-
-#if defined WIN32
-	#define PAGE_ALIGN(x) (x)
-
-	#define PROT_NONE  0x0 
-	#define PROT_READ  0x1 
-	#define PROT_WRITE 0x2 
-	#define PROT_EXEC  0x4 
-
-	// mprotect() implementation for Windows.
-	static int mprotect(void *addr, size_t len, int prot) {
-		DWORD new_prot;
-		DWORD old_prot;
-
-		if (prot == PROT_NONE) {
-			new_prot = PAGE_NOACCESS;
-		} else if (prot == PROT_READ) {
-			new_prot = PAGE_READONLY;
-		} else if ((prot & PROT_WRITE) && (prot & PROT_READ)) {
-			new_prot = PAGE_EXECUTE_READWRITE;
-		} else if ((prot & PROT_READ)) {
-			new_prot = PAGE_EXECUTE_READ;
-		} else if ((prot & PROT_EXEC)) {
-			new_prot = PAGE_EXECUTE;
-		}
-
-		return !VirtualProtect((LPVOID)addr, (SIZE_T)len, new_prot, &old_prot);
-	}
-
-#else
-	#define PAGE_ALIGN(x) (((int)x + sysconf(_SC_PAGESIZE) - 1) & ~(sysconf(_SC_PAGESIZE) - 1))
-#endif
 
 static logprintf_t logprintf;
 
@@ -92,20 +55,6 @@ static int AMXAPI amx_Exec_JIT(AMX *amx, cell *retval, int index) {
 	return AMX_ERR_NONE;
 }
 
-static void Hook(void *src, void *dst) {
-	// Set write permission.
-	mprotect((void*)PAGE_ALIGN(src), PAGE_ALIGN(5), PROT_READ | PROT_WRITE | PROT_EXEC);
-
-	// Write the JMP opcode.
-	static const uint8_t JMP_rel32 = 0xE9;
-	*reinterpret_cast<uint8_t*>(src) = JMP_rel32;
-
-	// Write the address (which is relative to next instruction).
-	int32_t src_addr = reinterpret_cast<int32_t>(src);
-	int32_t dst_addr = reinterpret_cast<int32_t>(dst);
-	*reinterpret_cast<int32_t*>(src_addr + 1) = dst_addr - (src_addr + 5);
-}
-
 PLUGIN_EXPORT unsigned int PLUGIN_CALL Supports() {
 	return SUPPORTS_VERSION | SUPPORTS_AMX_NATIVES;
 }
@@ -113,10 +62,8 @@ PLUGIN_EXPORT unsigned int PLUGIN_CALL Supports() {
 PLUGIN_EXPORT bool PLUGIN_CALL Load(void **ppData) {
 	logprintf = (logprintf_t)ppData[PLUGIN_DATA_LOGPRINTF];
 
-	Hook(((void**)ppData[PLUGIN_DATA_AMX_EXPORTS])[PLUGIN_AMX_EXPORT_Exec],
-	                 (void*)amx_Exec_JIT);
-	Hook(((void**)ppData[PLUGIN_DATA_AMX_EXPORTS])[PLUGIN_AMX_EXPORT_GetAddr],
-	                 (void*)amx_GetAddr_JIT);
+	new JumpX86(((void**)ppData[PLUGIN_DATA_AMX_EXPORTS])[PLUGIN_AMX_EXPORT_Exec], (void*)amx_Exec_JIT);
+	new JumpX86(((void**)ppData[PLUGIN_DATA_AMX_EXPORTS])[PLUGIN_AMX_EXPORT_GetAddr], (void*)amx_GetAddr_JIT);
 
 	logprintf("  JIT plugin v%s is OK.", PLUGIN_VERSION_STRING);
 
