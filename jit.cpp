8a1b9961f24e8ecf60f0bf2c7317693a27f54060
Sergey Zolotarev <zeex@rocketmail.com>
2012-03-23 21:00:13 +0600

    Fix AsmJit assertion failure on nested CompileFunction() calls

--- jit.cpp
+++ jit.cpp
@@ -131,15 +131,11 @@ static int GetNativeIndex(AMX *amx, cell address) {
 namespace jit {
 
 #define JIT_OVERRIDE_NATIVE(name) \
-	do { native_overrides_[#name] = &JIT::native_##name; } while (false);
+	do { native_overrides_[#name] = &JITAssembler::native_##name; } while (false);
 
-JIT::JIT(AMX *amx, cell *opcode_list)
+JITAssembler::JITAssembler(JIT *jit)
 	: AsmJit::Assembler(0)
-	, amx_(amx)
-	, amxhdr_(reinterpret_cast<AMX_HEADER*>(amx_->base))
-	, data_(amx_->data != 0 ? amx_->data : amx_->base + amxhdr_->dat)
-	, code_(amx_->base + amxhdr_->cod)
-	, opcode_list_(opcode_list)
+	, jit_(jit)
 {
 	JIT_OVERRIDE_NATIVE(float);
 	JIT_OVERRIDE_NATIVE(floatabs);
@@ -151,201 +147,15 @@ JIT::JIT(AMX *amx, cell *opcode_list)
 	JIT_OVERRIDE_NATIVE(floatlog);
 }
 
-JIT::~JIT() {
-	for (ProcMap::iterator iterator = proc_map_.begin(); iterator != proc_map_.end(); ++iterator) {
-		AsmJit::MemoryManager::getGlobal()->free(iterator->second);
-	}
-}
+void *JITAssembler::CompileFunction(cell address) {
+	AMX *amx = jit_->GetAmx();
+	AMX_HEADER *amxhdr = jit_->GetAmxHeader();
 
-void JIT::AnalyzeFunction(cell address, std::vector<AMXInstruction> &instructions) const {
-	const cell *cip = reinterpret_cast<cell*>(code_ + address);
-	bool seen_proc = false;
-
-	while (cip < reinterpret_cast<cell*>(data_)) {
-		cell opcode = *cip;
-
-		if (opcode_list_ != 0) {
-			for (int i = 0; i < NUM_AMX_OPCODES; i++) {
-				if (opcode_list_[i] == opcode) {
-					opcode = i;
-					break;
-				}
-			}
-		}
-
-		if (opcode == OP_PROC) {
-			// Start of function body...
-			if (seen_proc) {
-				return;
-			}
-			seen_proc = true;
-		}
-
-		AMXInstruction instr(static_cast<AMXOpcode>(opcode), cip);
-		instructions.push_back(instr);
-
-		switch (opcode) {
-		// Instructions with one operand.
-		case OP_LOAD_PRI:
-		case OP_LOAD_ALT:
-		case OP_LOAD_S_PRI:
-		case OP_LOAD_S_ALT:
-		case OP_LREF_PRI:
-		case OP_LREF_ALT:
-		case OP_LREF_S_PRI:
-		case OP_LREF_S_ALT:
-		case OP_LODB_I:
-		case OP_CONST_PRI:
-		case OP_CONST_ALT:
-		case OP_ADDR_PRI:
-		case OP_ADDR_ALT:
-		case OP_STOR_PRI:
-		case OP_STOR_ALT:
-		case OP_STOR_S_PRI:
-		case OP_STOR_S_ALT:
-		case OP_SREF_PRI:
-		case OP_SREF_ALT:
-		case OP_SREF_S_PRI:
-		case OP_SREF_S_ALT:
-		case OP_STRB_I:
-		case OP_LIDX_B:
-		case OP_IDXADDR_B:
-		case OP_ALIGN_PRI:
-		case OP_ALIGN_ALT:
-		case OP_LCTRL:
-		case OP_SCTRL:
-		case OP_PUSH_R:
-		case OP_PUSH_C:
-		case OP_PUSH:
-		case OP_PUSH_S:
-		case OP_STACK:
-		case OP_HEAP:
-		case OP_JREL:
-		case OP_JUMP:
-		case OP_JZER:
-		case OP_JNZ:
-		case OP_JEQ:
-		case OP_JNEQ:
-		case OP_JLESS:
-		case OP_JLEQ:
-		case OP_JGRTR:
-		case OP_JGEQ:
-		case OP_JSLESS:
-		case OP_JSLEQ:
-		case OP_JSGRTR:
-		case OP_JSGEQ:
-		case OP_SHL_C_PRI:
-		case OP_SHL_C_ALT:
-		case OP_SHR_C_PRI:
-		case OP_SHR_C_ALT:
-		case OP_ADD_C:
-		case OP_SMUL_C:
-		case OP_ZERO:
-		case OP_ZERO_S:
-		case OP_EQ_C_PRI:
-		case OP_EQ_C_ALT:
-		case OP_INC:
-		case OP_INC_S:
-		case OP_DEC:
-		case OP_DEC_S:
-		case OP_MOVS:
-		case OP_CMPS:
-		case OP_FILL:
-		case OP_HALT:
-		case OP_BOUNDS:
-		case OP_CALL:
-		case OP_SYSREQ_C:
-		case OP_PUSH_ADR:
-		case OP_SYSREQ_D:
-		case OP_SWITCH:			
-			cip += 2;
-			break;
-
-		// Instructions with no operands.
-		case OP_LOAD_I:
-		case OP_STOR_I:
-		case OP_LIDX:
-		case OP_IDXADDR:
-		case OP_MOVE_PRI:
-		case OP_MOVE_ALT:
-		case OP_XCHG:
-		case OP_PUSH_PRI:
-		case OP_PUSH_ALT:
-		case OP_POP_PRI:
-		case OP_POP_ALT:
-		case OP_PROC:
-		case OP_RET:
-		case OP_RETN:
-		case OP_CALL_PRI:
-		case OP_SHL:
-		case OP_SHR:
-		case OP_SSHR:
-		case OP_SMUL:
-		case OP_SDIV:
-		case OP_SDIV_ALT:
-		case OP_UMUL:
-		case OP_UDIV:
-		case OP_UDIV_ALT:
-		case OP_ADD:
-		case OP_SUB:
-		case OP_SUB_ALT:
-		case OP_AND:
-		case OP_OR:
-		case OP_XOR:
-		case OP_NOT:
-		case OP_NEG:
-		case OP_INVERT:
-		case OP_ZERO_PRI:
-		case OP_ZERO_ALT:
-		case OP_SIGN_PRI:
-		case OP_SIGN_ALT:
-		case OP_EQ:
-		case OP_NEQ:
-		case OP_LESS:
-		case OP_LEQ:
-		case OP_GRTR:
-		case OP_GEQ:
-		case OP_SLESS:
-		case OP_SLEQ:
-		case OP_SGRTR:
-		case OP_SGEQ:
-		case OP_INC_PRI:
-		case OP_INC_ALT:
-		case OP_INC_I:
-		case OP_DEC_PRI:
-		case OP_DEC_ALT:
-		case OP_DEC_I:
-		case OP_SYSREQ_PRI:
-		case OP_JUMP_PRI:
-		case OP_SWAP_PRI:
-		case OP_SWAP_ALT:
-		case OP_NOP:
-		case OP_BREAK:
-			cip++;
-			break;
-
-		// Special instructions.
-		case OP_CASETBL: {
-			cip++;
-			int num = *cip++;
-			cip += 2 * num + 1;
-			break;
-		}
-
-		default:
-			throw InvalidInstructionError(instr);
-		}
-	}
-}
-
-void *JIT::CompileFunction(cell address) {
-	proc_map_[address] = 0;
-
-	cell data = reinterpret_cast<cell>(data_);
-	cell code = reinterpret_cast<cell>(code_);
+	cell code = reinterpret_cast<cell>(jit_->GetAmxCode());
+	cell data = reinterpret_cast<cell>(jit_->GetAmxData());	
 
 	std::vector<AMXInstruction> instructions;
-	AnalyzeFunction(address, instructions);
+	jit_->AnalyzeFunction(address, instructions);
 
 	using AsmJit::byte_ptr;
 	using AsmJit::byte_ptr_abs;
@@ -371,7 +181,12 @@ void *JIT::CompileFunction(cell address) {
 	{
 		AMXInstruction &instr = *iterator;
 
+
+		static std::map<cell, bool> bound_labels;
+
 		cell cip = reinterpret_cast<cell>(instr.GetIP()) - code;
+
+		assert(bound_labels.find(cip) == bound_labels.end());		
 		bind(L(cip));
 
 		switch (instr.GetOpcode()) {
@@ -527,13 +342,13 @@ void *JIT::CompileFunction(cell address) {
 			// 3=STP, 4=STK, 5=FRM, 6=CIP (of the next instruction)
 			switch (instr.GetOperand()) {
 			case 0:
-				mov(eax, dword_ptr_abs(reinterpret_cast<void*>(&amxhdr_->cod)));
+				mov(eax, dword_ptr_abs(reinterpret_cast<void*>(&amxhdr->cod)));
 				break;
 			case 1:
-				mov(eax, dword_ptr_abs(reinterpret_cast<void*>(&amxhdr_->dat)));
+				mov(eax, dword_ptr_abs(reinterpret_cast<void*>(&amxhdr->dat)));
 				break;
 			case 2:
-				mov(eax, dword_ptr_abs(reinterpret_cast<void*>(&amx_->hea)));
+				mov(eax, dword_ptr_abs(reinterpret_cast<void*>(&amx->hea)));
 				break;
 			case 4:
 				lea(eax, dword_ptr(esp, -data));
@@ -551,7 +366,7 @@ void *JIT::CompileFunction(cell address) {
 			// 6=CIP
 			switch (instr.GetOperand()) {
 			case 2:
-				mov(dword_ptr_abs(reinterpret_cast<void*>(&amx_->hea)), eax);
+				mov(dword_ptr_abs(reinterpret_cast<void*>(&amx->hea)), eax);
 				break;
 			case 4:
 				lea(esp, dword_ptr(eax, data));
@@ -613,8 +428,8 @@ void *JIT::CompileFunction(cell address) {
 			break;
 		case OP_HEAP: // value
 			// ALT = HEA, HEA = HEA + value
-			mov(ecx, dword_ptr_abs(reinterpret_cast<void*>(&amx_->hea)));
-			add(dword_ptr_abs(reinterpret_cast<void*>(&amx_->hea)), instr.GetOperand());
+			mov(ecx, dword_ptr_abs(reinterpret_cast<void*>(&amx->hea)));
+			add(dword_ptr_abs(reinterpret_cast<void*>(&amx->hea)), instr.GetOperand());
 			break;
 		case OP_PROC:
 			// [STK] = FRM, STK = STK - cell size, FRM = STK
@@ -645,7 +460,7 @@ void *JIT::CompileFunction(cell address) {
 			// The address jumped to is relative to the current CIP,
 			// but the address on the stack is an absolute address.
 			cell fn_addr = instr.GetOperand() - code;
-			void *fn = GetFunction(fn_addr);
+			void *fn = jit_->GetFunction(fn_addr);
 			if (fn != 0) {
 				// The target function has been compiled.
 				call(fn);
@@ -653,7 +468,7 @@ void *JIT::CompileFunction(cell address) {
 				// The current function calls itself directly or indirectly.
 				push(esp);
 				push(fn_addr);
-				push(reinterpret_cast<int>(this));
+				push(reinterpret_cast<int>(jit_));
 				mov(edx, reinterpret_cast<int>(::CallFunction));
 			}
 			add(esp, dword_ptr(esp));
@@ -1049,7 +864,7 @@ void *JIT::CompileFunction(cell address) {
 			// call system service, service number in PRI
 			push(esp);
 			push(eax);
-			push(reinterpret_cast<int>(this));
+			push(reinterpret_cast<int>(jit_));
 			call(reinterpret_cast<void*>(::CallNativeFunction));
 			break;
 		case OP_SYSREQ_C:   // index
@@ -1058,12 +873,12 @@ void *JIT::CompileFunction(cell address) {
 			std::string native_name;
 			switch (instr.GetOpcode()) {
 				case OP_SYSREQ_C:
-					native_name = GetNativeName(amx_, instr.GetOperand());
+					native_name = GetNativeName(amx, instr.GetOperand());
 					break;
 				case OP_SYSREQ_D: {
-					int index = GetNativeIndex(amx_, instr.GetOperand());
+					int index = GetNativeIndex(amx, instr.GetOperand());
 					if (index != -1) {
-						native_name = GetNativeName(amx_, index);
+						native_name = GetNativeName(amx, index);
 					}
 					break;
 				}
@@ -1079,10 +894,10 @@ void *JIT::CompileFunction(cell address) {
 			}
 		ordinary_native:
 			push(esp);
-			push(reinterpret_cast<int>(amx_));
+			push(reinterpret_cast<int>(amx));
 			switch (instr.GetOpcode()) {
 				case OP_SYSREQ_C:					
-					call(reinterpret_cast<void*>(GetNativeAddress(amx_, instr.GetOperand())));
+					call(reinterpret_cast<void*>(GetNativeAddress(amx, instr.GetOperand())));
 					break;
 				case OP_SYSREQ_D:
 					call(reinterpret_cast<void*>(instr.GetOperand()));
@@ -1189,20 +1004,17 @@ void *JIT::CompileFunction(cell address) {
 		}		
 	}
 
-	void *fn = make();
-	clear();
-	proc_map_[address] = fn;	
-	return fn;
+	return make();
 }
 
-void JIT::halt(cell code) {
-	mov(AsmJit::dword_ptr_abs(reinterpret_cast<void*>(&amx_->error)), code);
-	mov(AsmJit::esp, AsmJit::dword_ptr_abs(reinterpret_cast<void*>(&halt_esp_)));
-	mov(AsmJit::ebp, AsmJit::dword_ptr_abs(reinterpret_cast<void*>(&halt_ebp_)));
+void JITAssembler::halt(cell code) {
+	mov(AsmJit::dword_ptr_abs(reinterpret_cast<void*>(&jit_->GetAmx()->error)), code);
+	mov(AsmJit::esp, AsmJit::dword_ptr_abs(reinterpret_cast<void*>(&jit_->halt_esp_)));
+	mov(AsmJit::ebp, AsmJit::dword_ptr_abs(reinterpret_cast<void*>(&jit_->halt_ebp_)));
 	ret();
 }
 
-void JIT::native_float() {
+void JITAssembler::native_float() {
 	fild(dword_ptr(AsmJit::esp, 4));
 	sub(AsmJit::esp, 4);
 	fstp(dword_ptr(AsmJit::esp));
@@ -1210,7 +1022,7 @@ void JIT::native_float() {
 	add(AsmJit::esp, 4);
 }
 
-void JIT::native_floatabs() {
+void JITAssembler::native_floatabs() {
 	using AsmJit::dword_ptr;
 	fld(dword_ptr(AsmJit::esp, 4));
 	fabs();
@@ -1220,7 +1032,7 @@ void JIT::native_floatabs() {
 	add(AsmJit::esp, 4);
 }
 
-void JIT::native_floatadd() {
+void JITAssembler::native_floatadd() {
 	fld(dword_ptr(AsmJit::esp, 4));
 	fadd(dword_ptr(AsmJit::esp, 8));
 	sub(AsmJit::esp, 4);
@@ -1229,7 +1041,7 @@ void JIT::native_floatadd() {
 	add(AsmJit::esp, 4);
 }
 
-void JIT::native_floatsub() {
+void JITAssembler::native_floatsub() {
 	fld(dword_ptr(AsmJit::esp, 4));
 	fsub(dword_ptr(AsmJit::esp, 8));
 	sub(AsmJit::esp, 4);
@@ -1238,7 +1050,7 @@ void JIT::native_floatsub() {
 	add(AsmJit::esp, 4);
 }
 
-void JIT::native_floatmul() {
+void JITAssembler::native_floatmul() {
 	fld(dword_ptr(AsmJit::esp, 4));
 	fmul(dword_ptr(AsmJit::esp, 8));
 	sub(AsmJit::esp, 4);
@@ -1247,7 +1059,7 @@ void JIT::native_floatmul() {
 	add(AsmJit::esp, 4);
 }
 
-void JIT::native_floatdiv() {
+void JITAssembler::native_floatdiv() {
 	fld(dword_ptr(AsmJit::esp, 4));
 	fdiv(dword_ptr(AsmJit::esp, 8));
 	sub(AsmJit::esp, 4);
@@ -1256,7 +1068,7 @@ void JIT::native_floatdiv() {
 	add(AsmJit::esp, 4);
 }
 
-void JIT::native_floatsqroot() {
+void JITAssembler::native_floatsqroot() {
 	fld(dword_ptr(AsmJit::esp, 4));
 	fsqrt();
 	sub(AsmJit::esp, 4);
@@ -1265,7 +1077,7 @@ void JIT::native_floatsqroot() {
 	add(AsmJit::esp, 4);
 }
 
-void JIT::native_floatlog() {
+void JITAssembler::native_floatlog() {
 	fld1();
 	fld(dword_ptr(AsmJit::esp, 8));
 	fyl2x();
@@ -1277,6 +1089,223 @@ void JIT::native_floatlog() {
 	fstp(dword_ptr(AsmJit::esp));
 	mov(AsmJit::eax, dword_ptr(AsmJit::esp));
 	add(AsmJit::esp, 4);
+}
+
+AsmJit::Label &JITAssembler::L(cell address, const std::string &tag) {
+	LabelMap::iterator iterator = label_map_.find(TaggedAddress(address, tag));
+	if (iterator != label_map_.end()) {
+		return iterator->second;
+	} else {
+		std::pair<LabelMap::iterator, bool> where = 
+				label_map_.insert(std::make_pair(TaggedAddress(address, tag), newLabel()));
+		return where.first->second;
+	}
+}
+
+JIT::JIT(AMX *amx, cell *opcode_list)
+	: amx_(amx)
+	, amxhdr_(reinterpret_cast<AMX_HEADER*>(amx_->base))
+	, data_(amx_->data != 0 ? amx_->data : amx_->base + amxhdr_->dat)
+	, code_(amx_->base + amxhdr_->cod)
+	, opcode_list_(opcode_list)
+{
+}
+
+JIT::~JIT() {
+	for (ProcMap::iterator iterator = proc_map_.begin(); iterator != proc_map_.end(); ++iterator) {
+		AsmJit::MemoryManager::getGlobal()->free(iterator->second);
+	}
+}
+
+void JIT::AnalyzeFunction(cell address, std::vector<AMXInstruction> &instructions) const {
+	const cell *cip = reinterpret_cast<cell*>(code_ + address);
+	bool seen_proc = false;
+
+	while (cip < reinterpret_cast<cell*>(data_)) {
+		cell opcode = *cip;
+
+		if (opcode_list_ != 0) {
+			for (int i = 0; i < NUM_AMX_OPCODES; i++) {
+				if (opcode_list_[i] == opcode) {
+					opcode = i;
+					break;
+				}
+			}
+		}
+
+		if (opcode == OP_PROC) {
+			// Start of function body...
+			if (seen_proc) {
+				return;
+			}
+			seen_proc = true;
+		}
+
+		AMXInstruction instr(static_cast<AMXOpcode>(opcode), cip);
+		instructions.push_back(instr);
+
+		switch (opcode) {
+		// Instructions with one operand.
+		case OP_LOAD_PRI:
+		case OP_LOAD_ALT:
+		case OP_LOAD_S_PRI:
+		case OP_LOAD_S_ALT:
+		case OP_LREF_PRI:
+		case OP_LREF_ALT:
+		case OP_LREF_S_PRI:
+		case OP_LREF_S_ALT:
+		case OP_LODB_I:
+		case OP_CONST_PRI:
+		case OP_CONST_ALT:
+		case OP_ADDR_PRI:
+		case OP_ADDR_ALT:
+		case OP_STOR_PRI:
+		case OP_STOR_ALT:
+		case OP_STOR_S_PRI:
+		case OP_STOR_S_ALT:
+		case OP_SREF_PRI:
+		case OP_SREF_ALT:
+		case OP_SREF_S_PRI:
+		case OP_SREF_S_ALT:
+		case OP_STRB_I:
+		case OP_LIDX_B:
+		case OP_IDXADDR_B:
+		case OP_ALIGN_PRI:
+		case OP_ALIGN_ALT:
+		case OP_LCTRL:
+		case OP_SCTRL:
+		case OP_PUSH_R:
+		case OP_PUSH_C:
+		case OP_PUSH:
+		case OP_PUSH_S:
+		case OP_STACK:
+		case OP_HEAP:
+		case OP_JREL:
+		case OP_JUMP:
+		case OP_JZER:
+		case OP_JNZ:
+		case OP_JEQ:
+		case OP_JNEQ:
+		case OP_JLESS:
+		case OP_JLEQ:
+		case OP_JGRTR:
+		case OP_JGEQ:
+		case OP_JSLESS:
+		case OP_JSLEQ:
+		case OP_JSGRTR:
+		case OP_JSGEQ:
+		case OP_SHL_C_PRI:
+		case OP_SHL_C_ALT:
+		case OP_SHR_C_PRI:
+		case OP_SHR_C_ALT:
+		case OP_ADD_C:
+		case OP_SMUL_C:
+		case OP_ZERO:
+		case OP_ZERO_S:
+		case OP_EQ_C_PRI:
+		case OP_EQ_C_ALT:
+		case OP_INC:
+		case OP_INC_S:
+		case OP_DEC:
+		case OP_DEC_S:
+		case OP_MOVS:
+		case OP_CMPS:
+		case OP_FILL:
+		case OP_HALT:
+		case OP_BOUNDS:
+		case OP_CALL:
+		case OP_SYSREQ_C:
+		case OP_PUSH_ADR:
+		case OP_SYSREQ_D:
+		case OP_SWITCH:			
+			cip += 2;
+			break;
+
+		// Instructions with no operands.
+		case OP_LOAD_I:
+		case OP_STOR_I:
+		case OP_LIDX:
+		case OP_IDXADDR:
+		case OP_MOVE_PRI:
+		case OP_MOVE_ALT:
+		case OP_XCHG:
+		case OP_PUSH_PRI:
+		case OP_PUSH_ALT:
+		case OP_POP_PRI:
+		case OP_POP_ALT:
+		case OP_PROC:
+		case OP_RET:
+		case OP_RETN:
+		case OP_CALL_PRI:
+		case OP_SHL:
+		case OP_SHR:
+		case OP_SSHR:
+		case OP_SMUL:
+		case OP_SDIV:
+		case OP_SDIV_ALT:
+		case OP_UMUL:
+		case OP_UDIV:
+		case OP_UDIV_ALT:
+		case OP_ADD:
+		case OP_SUB:
+		case OP_SUB_ALT:
+		case OP_AND:
+		case OP_OR:
+		case OP_XOR:
+		case OP_NOT:
+		case OP_NEG:
+		case OP_INVERT:
+		case OP_ZERO_PRI:
+		case OP_ZERO_ALT:
+		case OP_SIGN_PRI:
+		case OP_SIGN_ALT:
+		case OP_EQ:
+		case OP_NEQ:
+		case OP_LESS:
+		case OP_LEQ:
+		case OP_GRTR:
+		case OP_GEQ:
+		case OP_SLESS:
+		case OP_SLEQ:
+		case OP_SGRTR:
+		case OP_SGEQ:
+		case OP_INC_PRI:
+		case OP_INC_ALT:
+		case OP_INC_I:
+		case OP_DEC_PRI:
+		case OP_DEC_ALT:
+		case OP_DEC_I:
+		case OP_SYSREQ_PRI:
+		case OP_JUMP_PRI:
+		case OP_SWAP_PRI:
+		case OP_SWAP_ALT:
+		case OP_NOP:
+		case OP_BREAK:
+			cip++;
+			break;
+
+		// Special instructions.
+		case OP_CASETBL: {
+			cip++;
+			int num = *cip++;
+			cip += 2 * num + 1;
+			break;
+		}
+
+		default:
+			throw InvalidInstructionError(instr);
+		}
+	}
+}
+
+void *JIT::CompileFunction(cell address) {
+	proc_map_[address] = 0;
+
+	JITAssembler as(this);
+	void *fn = as.CompileFunction(address);
+
+	proc_map_[address] = fn;
+	return fn;
 }
 
 void *JIT::GetFunction(cell address) {
@@ -1385,17 +1414,6 @@ int JIT::CallPublicFunction(int index, cell *retval) {
 cell JIT::CallNativeFunction(int index, cell *params) {
 	AMX_NATIVE native = reinterpret_cast<AMX_NATIVE>(GetNativeAddress(amx_, index));
 	return native(amx_, params);
-}
-
-AsmJit::Label &JIT::L(cell address, const std::string &tag) {
-	LabelMap::iterator iterator = label_map_.find(TaggedAddress(address, tag));
-	if (iterator != label_map_.end()) {
-		return iterator->second;
-	} else {
-		std::pair<LabelMap::iterator, bool> where = 
-				label_map_.insert(std::make_pair(TaggedAddress(address, tag), newLabel()));
-		return where.first->second;
-	}
 }
 
 } // namespace jit
