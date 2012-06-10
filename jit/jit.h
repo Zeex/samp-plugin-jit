6f0bd8f824e553e756e0e9451af83cfd8f95b9a5
Sergey Zolotarev <zeex@rocketmail.com>
2012-06-10 16:27:19 +0600

    Completely remove canOverwriteRegister()

--- jit/jit.h
+++ jit/jit.h
@@ -363,10 +363,6 @@ private:
 	// Get addresses of all functions and jump instructions' destinations.
 	bool collectJumpAddresses(std::set<cell> &refs) const;
 
-	// See if we can safely write to a register because it is overwritten any way by
-	// following instructions.
-	bool canOverwriteRegister(cell address, AMXRegister reg) const;
-
 	// Sets a label at the specified AMX address. The address should be relative to the
 	// start of the code section.
 	AsmJit::Label &L(AsmJit::X86Assembler *as, cell address);
