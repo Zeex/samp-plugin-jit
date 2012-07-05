3a8f0a4277aef196adc06e95fdc46cd7e9868864
Zeex <zeex@rocketmail.com>
2012-07-06 04:37:58 +0600

    New directory structure

---
+++ src/os-unix.cpp
@@ -0,0 +1,63 @@
+// Copyright (c) 2012, Zeex
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice, this
+//    list of conditions and the following disclaimer.
+// 2. Redistributions in binary form must reproduce the above copyright notice,
+//    this list of conditions and the following disclaimer in the documentation
+//    and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+// ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#include "os.h"
+
+#include <cstring>
+#include <string>
+#include <vector>
+
+#include <dirent.h>
+#include <fnmatch.h>
+
+#ifndef _GNU_SOURCE
+	#define _GNU_SOURCE 1 // for dladdr()
+#endif
+#include <dlfcn.h> 
+
+const char os::kDirSepChar = '/';
+
+std::string os::GetModulePath(void *address, std::size_t maxLength) {
+	std::vector<char> name(maxLength + 1);
+	if (address != 0) {
+		Dl_info info;
+		dladdr(address, &info);
+		strncpy(&name[0], info.dli_fname, maxLength);
+	}	
+	return std::string(&name[0]);
+}
+
+void os::ListDirectoryFiles(const std::string &directory, const std::string &pattern,
+		bool (*callback)(const char *, void *), void *userData) 
+{
+	DIR *dp;
+	if ((dp = opendir(directory.c_str())) != 0) {
+		struct dirent *dirp;
+		while ((dirp = readdir(dp)) != 0) {
+			if (!fnmatch(pattern.c_str(), dirp->d_name, FNM_CASEFOLD | FNM_NOESCAPE | FNM_PERIOD)) {
+				callback(dirp->d_name, userData);
+			}
+		}
+		closedir(dp);
+	}
+}
---
+++ src/os-win32.cpp
@@ -0,0 +1,58 @@
+// Copyright (c) 2012, Zeex
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice, this
+//    list of conditions and the following disclaimer.
+// 2. Redistributions in binary form must reproduce the above copyright notice,
+//    this list of conditions and the following disclaimer in the documentation
+//    and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+// ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#include "os.h"
+
+#include <string>
+#include <vector>
+
+#include <Windows.h>
+
+const char os::kDirSepChar = '\\';
+
+#undef GetModulePath
+
+std::string os::GetModulePath(void *address, std::size_t maxLength) {
+	std::vector<char> name(maxLength + 1);
+	if (address != 0) {
+		MEMORY_BASIC_INFORMATION mbi;
+		VirtualQuery(address, &mbi, sizeof(mbi));
+		GetModuleFileName((HMODULE)mbi.AllocationBase, &name[0], maxLength);
+	}
+	return std::string(&name[0]);
+}
+
+void os::ListDirectoryFiles(const std::string &directory, const std::string &pattern,
+		bool (*callback)(const char *, void *), void *userData) 
+{
+	WIN32_FIND_DATA findFileData;
+	HANDLE hFindFile = FindFirstFile((directory + kDirSepChar + pattern).c_str(), &findFileData);
+	if (hFindFile != INVALID_HANDLE_VALUE) {
+		do {
+			if (!(findFileData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)) {
+				callback(findFileData.cFileName, userData);
+			}
+		} while (FindNextFile(hFindFile, &findFileData) != 0);
+		FindClose(hFindFile);
+	}
+}
