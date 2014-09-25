905ac6bc1b89f5bba3b0107a6514f551d7f81a36
Zeex <zeex@rocketmail.com>
2014-09-26 04:17:14 +0700

    Remove old asmjit subtree

--- deps/asmjit/asmjit/scripts/configure-mac-xcode.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-ASMJIT_SCRIPTS_DIR=`pwd`
-ASMJIT_BUILD_DIR="build_xcode"
-
-mkdir ../${ASMJIT_BUILD_DIR}
-cd ../${ASMJIT_BUILD_DIR}
-cmake .. -G"Xcode" -DASMJIT_BUILD_SAMPLES=1
-cd ${ASMJIT_SCRIPTS_DIR}
